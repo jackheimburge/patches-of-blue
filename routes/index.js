f6a100df598248cdf078814ce31e07bd4a6cf02b
centipedejam <jheimburge@gmail.com>
2023-09-23 17:33:57 -0700

    add routes, add links to navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Patches of Blue' });
 });
+
+router.get('/injustice-the-book', function(req, res) {
+  res.render('inJustice', {title: 'inJustice - the book'})
+})
 
 module.exports = router;
